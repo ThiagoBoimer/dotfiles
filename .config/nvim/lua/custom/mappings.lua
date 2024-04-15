ea3084495128d97818ba4c3785a1ee8ae0684c98
ThiagoBoimer <thiago_boimer@hotmail.com>
2024-04-15 12:28:40 -0300

    Adding nvim

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,56 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dso"] = {
+      "<cmd> DapUIStepOver <CR>",
+      "Step over"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function ()
+        require('dap-go').debut_test()
+      end,
+      "Debug go test"
+    }
+  },
+  ["<leader>dgl"] = {
+    function ()
+      require('dap-go').debug_last()
+    end,
+    "Debug last go test"
+  }
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    },
+  }
+}
+
+return M
